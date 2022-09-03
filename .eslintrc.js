695057d9c2e15d3d839b17124ea0a893cb866188
xianyuntang <xt1800i@gmail.com>
2022-09-04 02:15:38 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@eove/eslint-config-electron",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "simple-import-sort",
+    "no-relative-import-paths",
+  ],
+
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-relative-import-paths/no-relative-import-paths": [
+      "error",
+      {
+        allowSameFolder: true,
+        rootDir: "src",
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  ignorePatterns: ["/scripts/*"],
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Electron 20 + TypeScript 4 + Vite 2 + React 18 + Tailwind 3 + Eslint starter
+
+
+
+
+## Reference
+
+https://github.com/twstyled/electron-vite-react
+
